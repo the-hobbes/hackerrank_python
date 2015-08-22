82e950a0babc364dbfad5bb12ab26a479d220220
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-08-21 21:53:57 -0700

    Added two python domain solutions, percentage finder and lists exercise

--- finding_percentage.py
+++ finding_percentage.py
@@ -7,13 +7,18 @@
 
 from __future__ import division  # for python 3 style division
 
-students = {}
-n = input()
-for i in range(0,n):
-    s = raw_input()
-    a = s.split(" ")
-    int_list = [float(n) for n in a[1:]]
-    average = sum(int_list) / len(int_list)
-    students[a[0]] = average
-target = raw_input()
-print "{0:.2f}".format(students[target])
+def main():
+	students = {}
+	n = input() # first line gives how many lines of input
+	for i in range(0,n): # each line of input
+	    s = raw_input()
+	    a = s.split(" ")
+	    int_list = [float(n) for n in a[1:]]
+	    average = sum(int_list) / len(int_list)
+	    students[a[0]] = average
+	target = raw_input() # last line gives the target student
+	print "{0:.2f}".format(students[target])
+
+if __name__ == '__main__':
+	main()
+
---
+++ lists.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+'''
+    Finding the percentage
+    https://www.hackerrank.com/challenges/finding-the-percentage
+'''
+
+L = []
+
+def runCommand(command, args):
+	int_list = None
+	if len(args) > 0 and args[0] != "":
+		int_list = [int(arg) for arg in args]
+
+	if command == "insert":
+		if int_list:
+			L.insert(int_list[0], int_list[1])
+	elif command == "print":
+		print L
+	elif command == "remove":
+		for i in int_list:
+			L.remove(i)
+	elif command == "append":
+		for i in int_list:
+			L.append(i)
+	elif command == "sort":
+		L.sort()
+	elif command == "pop":
+		if int_list:
+			for i in int_list:
+				L.pop(i)
+		else:
+			L.pop()
+	elif command == "reverse":
+		L.reverse()
+	else:
+		print "Command %s not found" % command
+
+def main():
+	n = input()
+	for i in range(0,n):
+		line = raw_input()
+		line = line.split(" ")
+		command = line[0]
+		args = line[1:]
+		runCommand(command, args)
+
+if __name__ == '__main__':
+	main()
