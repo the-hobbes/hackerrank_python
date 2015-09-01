4098c02e380862ecc1a15211f05b7c79ac22127b
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-08-31 19:52:40 -0700

    working on solution to disjoint set problem

---
+++ no_idea.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+'''
+	https://www.hackerrank.com/challenges/no-idea
+'''
+
+def main():
+	happiness = 0
+	n, m = raw_input().split(" ")
+	candidates = raw_input().split(" ")
+	aLike = set(raw_input().split(" "))
+	bDislike = set(raw_input().split(" "))
+
+	for item in candidates:
+		if item in aLike:
+			happiness += 1
+		elif item in bDislike:
+			happiness -= 1
+
+	print happiness
+
+if __name__ == '__main__':
+	main()
