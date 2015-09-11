c7cab5c1d9dd965e807c17fdd616841ba30583d4
Phelan Vendeville <phelan.vendeville@gmail.com>
2015-09-10 19:28:32 -0700

    full solution to piling up

--- piling_up.py
+++ piling_up.py
@@ -10,16 +10,21 @@ from collections import deque
 
 def computeVerticalStack(lengthOfTestCase, testCase):
     exit = False
+    top = 0
     while not exit:
         if len(testCase) == 0:
             break
         else:
-            maximum = max(testCase)
+            maximum = max(testCase[0], testCase[-1])
+            if top == 0:
+                top = maximum
+            elif top > maximum:
+                top = maximum
             left = testCase[0]
             right = testCase[-1]
-            if left == maximum:
+            if left == top:
                 testCase.popleft()
-            elif right == maximum:
+            elif right == top:
                 testCase.pop()
             else:
                 exit = True
@@ -32,7 +37,7 @@ def main():
     numberOfTestCases = input()
     for i in range(0, numberOfTestCases):
         lengthOfTestCase = input()
-        testCase = raw_input().split(" ")
+        testCase = raw_input().strip().split(" ")
         intCase = deque([int(x) for x in testCase])
         print computeVerticalStack(lengthOfTestCase, intCase)
 
