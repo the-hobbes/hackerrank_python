31cbb063e4b632c907db2dc0c2c2323d3e576ec1
Phelan Vendeville <phelan.vendeville@gmail.com>
2016-05-15 08:57:52 -0700

    finish itertools combinations problem

--- itertools_combinations.py
+++ itertools_combinations.py
@@ -8,10 +8,12 @@ from itertools import combinations
 
 def main():
     s, k = raw_input().strip().split(' ')
+    s = ''.join(sorted(s))
     k = int(k)
 
     for i in range(1, k+1):
-        print sorted(list(combinations(s, i)))
+        for y in sorted(list(combinations(s, i))):
+            print ''.join(map(str,sorted(y)))
 
 if __name__ == '__main__':
     main()
